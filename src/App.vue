bd752f135a233ecd2abd9efaeddab4c544db70c7
ChiaraS97 <chiara.sucato97@gmail.com>
2023-04-26 12:02:32 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HeaderComponent/>
+  <MainComponent/>
+  <FooterComponent/>
+</template>
+
+<script>
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+
+  export default {
+    name: "App",
+    components: { HeaderComponent, MainComponent, FooterComponent }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
